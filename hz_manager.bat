70f3312d0ee0d9eb64ee2c598f67d442822b5e35
aisaka-taiga <qwerty0204@gmail.com>
2019-03-23 10:53:06 +0900

    Add files via upload

---
+++ hz_manager.bat
@@ -0,0 +1,6 @@
+@echo off
+::wmic PATH Win32_videocontroller get currentrefreshrate
+IF EXIST hz_manager.bat echo F | XCOPY "hz_manager.bat" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\hz_manager.bat" /f /s /h /e /d /y
+::for /f %%i in ('wmic path Win32_VideoController get CurrentHorizontalResolution^,CurrentVerticalResolution /value ^| find "="') do set "%%i"
+::echo your screen is %CurrentHorizontalResolution% * %CurrentVerticalResolution% pixels
+C:\ChangeScreenResolution.exe /f=100 /d=0
